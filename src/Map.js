7e6731873efc2c1c2453b9a7be2daa3eb5370c39
Stilefano <stefanobucci@Mac-mini.local>
2020-03-10 19:13:45 +0100

    Wip on marker

--- src/Map.js
+++ src/Map.js
@@ -5,8 +5,16 @@ import {
     Marker,
     TileLayer
 } from "react-leaflet"
-
+import L from "leaflet"
 import useGetCoordinates from "./Hooks/useGetCoordinates"
+const svg =
+    '<svg height="100" width="100"> <circle cx="50" cy="50" r="40" stroke="black" fill="red" /> Sorry, your browser does not support inline SVG. </svg>' /* insert your own svg */
+const iconUrl = "data:image/svg+xml;base64," + btoa(svg)
+
+const icon = L.divIcon({
+    iconSize: [25, 36],
+    html: `<svg width="135px" height="100px"> <circle cx="67" cy="50" r="10" fill="red" /></svg>`
+})
 
 export default function Map() {
     const coordinates = useGetCoordinates()
@@ -22,6 +30,7 @@ export default function Map() {
                     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                     {coordinates.map(c => (
                         <Marker
+                            icon={icon}
                             position={[
                                 parseFloat(c.latlng[0]),
                                 parseFloat(c.latlng[1])
