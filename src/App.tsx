f855006ab0d214cf19884f3891e0cefec7588fab
Stilefano <stefanobucci@Mac-mini.local>
2020-03-10 18:05:41 +0100

    remove log

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css"
 import Map from "./Map"
 
 function App() {
-    console.log(process.env.REACT_APP_HOST, "ahaha")
     return (
         <div className="App">
             <Map />
