f1cdfdcacee89e86cc31208244c3480841eb3730
Stilefano <stefanobucci@Mac-mini.station>
2020-03-19 09:52:54 +0100

    Fix latlng undefined

--- src/Map.js
+++ src/Map.js
@@ -31,30 +31,37 @@ export default function Map() {
             ) : (
                 <LeafletMap center={[41.8719, 12.5674]} zoom={3}>
                     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                    {coordinates.map(c => (
-                        <Marker
-                            key={c.country_name}
-                            icon={icon(c)}
-                            position={[
-                                parseFloat(c.latlng[0]),
-                                parseFloat(c.latlng[1])
-                            ]}
-                        >
-                            <LeafletPopup>
-                                <h4>{c.country_name}</h4>
-                                <p>
-                                    Cases: <strong>{c.cases}</strong> <br />
-                                    Deaths: <strong>{c.deaths}</strong> <br />
-                                    Total recovered:{" "}
-                                    <strong>{c.total_recovered}</strong> <br />
-                                    New deaths: <strong>{c.new_deaths}</strong>
-                                    <br />
-                                    Serious/Critical:
-                                    <strong>{c.serious_critical}</strong>
-                                </p>
-                            </LeafletPopup>
-                        </Marker>
-                    ))}
+                    {coordinates.map(c =>
+                        !!c.latlng ? (
+                            <Marker
+                                key={c.country_name}
+                                icon={icon(c)}
+                                position={[
+                                    parseFloat(c.latlng[0]),
+                                    parseFloat(c.latlng[1])
+                                ]}
+                            >
+                                <LeafletPopup>
+                                    <h4>{c.country_name}</h4>
+                                    <p>
+                                        Cases: <strong>{c.cases}</strong> <br />
+                                        Deaths: <strong>{c.deaths}</strong>{" "}
+                                        <br />
+                                        Total recovered:{" "}
+                                        <strong>
+                                            {c.total_recovered}
+                                        </strong>{" "}
+                                        <br />
+                                        New deaths:{" "}
+                                        <strong>{c.new_deaths}</strong>
+                                        <br />
+                                        Serious/Critical:
+                                        <strong>{c.serious_critical}</strong>
+                                    </p>
+                                </LeafletPopup>
+                            </Marker>
+                        ) : null
+                    )}
                 </LeafletMap>
             )}
         </>
